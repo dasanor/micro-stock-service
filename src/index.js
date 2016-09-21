746da2a3e8073244f99c7fb9c9e70f171c587c65
Nicolás Cornaglia <ncornag@gmail.com>
2016-09-21 23:31:45 +0200

    Use addOperation insted of the old add.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ require(base.config.get('models:stockModel'))(base);
 require(base.config.get('models:reserveModel'))(base);
 
 // Add operations
-base.services.add(require('./operations/create')(base));
-base.services.add(require('./operations/get')(base));
-base.services.add(require('./operations/update')(base));
-base.services.add(require('./operations/reserve')(base));
-base.services.add(require('./operations/unreserve')(base));
+base.services.addOperation(require('./operations/create')(base));
+base.services.addOperation(require('./operations/get')(base));
+base.services.addOperation(require('./operations/update')(base));
+base.services.addOperation(require('./operations/reserve')(base));
+base.services.addOperation(require('./operations/unreserve')(base));
