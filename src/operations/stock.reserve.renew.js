a505c495a5c8fe91281d22f42fb8dbb9366d7d6f
david.sanchez <david.sanchez@thinkwrap.com>
2016-11-22 16:59:16 +0100

    MB-111 added reserve.renew operation

---
+++ src/operations/stock.reserve.renew.js
@@ -0,0 +1,50 @@
+const moment = require('moment');
+/**
+ * ## `stock.reserve.renew` operation factory
+ *
+ * @param {base} Object The microbase object
+ * @return {Function} The operation factory
+ */
+function opFactory(base) {
+  const reserveActive = base.config.get('reserveActive');
+  const minutesToReserve = base.config.get('minutesToReserve');
+  const allowReserveTimeOverwrite = base.config.get('allowReserveTimeOverwrite');
+
+  const op = {
+    validator: {
+      schema: require(base.config.get('schemas:renewReserve')),
+    },
+    handler: (msg, reply) => {
+      if (reserveActive) {
+        let id = msg.id;
+
+        base.db.models.Reserve
+          .findOne({ _id: id })
+          .exec()
+          .then(reserve => {
+            if (!reserve) throw base.utils.Error('reserve_not_found', id);
+
+            const minutesTo = allowReserveTimeOverwrite ? msg.reserveStockForMinutes : minutesToReserve;
+            const expirationTime = moment().add(minutesTo, 'minutes').toDate();
+
+            reserve.expirationTime = expirationTime;
+
+            return reserve.save();
+          })
+          .then(savedReserve => {
+            if (base.logger.isDebugEnabled()) base.logger.debug(`[stock] reserve ${savedReserve._id} renewed`);
+
+            return reply(base.utils.genericResponse({ reserve: savedReserve.toClient() }));
+          })
+          .catch(error => {
+            reply(base.utils.genericResponse(null, error))
+          });
+      } else {
+        return reply(base.utils.genericResponse({warning: 'reserve_not_renewed'}));
+      }
+    }
+  };
+  return op;
+}
+
+module.exports = opFactory;
